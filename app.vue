db679292633e601f6e319189159e3f51334e33f8
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-11-08 23:09:21 +0100

    add tailwind and container

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<Container>
+			<h1 class="text-2xl font-bold">Hello World</h1>
+		</Container>
+	</div>
 </template>
---
+++ components/Container.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="container mx-auto bg-gray-200 h-screen">
+		<slot />
+	</div>
+</template>
