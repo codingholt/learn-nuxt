fe77917eeb45e25cf212cf38783772a6c2e81e2d
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-11-09 12:09:52 +0100

    fixed container

--- components/Container.vue
+++ components/Container.vue
@@ -2,8 +2,8 @@
 	<div
 		className="dark:bg-black dark:text-white duration-1000 overflow-y-auto relative h-screen flex flex-col"
 	>
-		<div class="container mx-auto bg-gray-500">
-			<div className="container mx-auto relative h-screen flex-1">
+		<div class="container mx-auto bg-gray-500 h-screen">
+			<div className="container mx-auto relative h-full flex-1">
 				<div class="h-full mt-5">
 					<slot />
 				</div>
